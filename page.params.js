23427c388d956f2bb38aba52161a68c6f0ed51bc
Matt Carter <m@ttcarter.com>
2012-12-21 17:44:20 +1000

    Added late bind functionality to rewrite the document hash - preserving parameters between document refreshes

--- page.params.js
+++ page.params.js
@@ -5,24 +5,35 @@
 * @author Matt Carter <m@ttcarter.com>
 */
 (function($, window, undefined) {
-	$(document).bind('pagebeforechange', function(e, data) {
-		var url = (typeof data.toPage == 'string') ? data.toPage : window.location;
-		var urlparsed = /^(.*?)#(.+?)\?(.*)$/.exec(url);
+	$(document)
+		.bind('pagebeforechange', function(e, data) {
+			if (typeof data.toPage != 'string')
+				return;
+			var url = (typeof data.toPage == 'string') ? data.toPage : window.location;
+			var urlparsed = /^(.*?)#(.+?)\?(.*)$/.exec(url);
+			console.log('PBC', url);
 
-		data.params = [];
-		if (urlparsed) {
-			var baseurl = urlparsed[1];
-			var hash = urlparsed[2];
-			var keypairs = urlparsed[3].split('&');
+			data.params = [];
+			if (urlparsed) {
+				var baseurl = urlparsed[1];
+				var hash = urlparsed[2];
+				var keypairs = urlparsed[3].split('&');
+				data.paramString = '#' + hash + '?' + keypairs;
 
-			for (var i = 0; i < keypairs.length; i++) {
-				var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
-				if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
-					data.params[keypairs[i]] = 0;
-				} else // Valid key=val pair
-					data.params[keyval[1]] = keyval[2];
-			}
-			data.toPage = $('#' + hash);
-		}
-	});
+				for (var i = 0; i < keypairs.length; i++) {
+					var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
+					if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
+						data.params[keypairs[i]] = 0;
+					} else // Valid key=val pair
+						data.params[keyval[1]] = keyval[2];
+				}
+				data.toPage = $('#' + hash);
+			} else
+				data.paramString = false;
+		})
+		.bind('pagechange', function(e, data) {
+			console.log('PC', data);
+			if (data.paramString)
+				document.location.hash = data.paramString;
+		});
 })(jQuery, window);
