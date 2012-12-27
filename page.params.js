12dd34e5af50854819e96def1591ea0f231e5270
Matt Carter <m@ttcarter.com>
2012-12-27 16:51:30 +1000

    Replaced jQM hash changer and replaced it with our own which handles parameters - this fix stops pagechange being called twice

--- page.params.js
+++ page.params.js
@@ -4,27 +4,35 @@
 * @url https://github.com/hash-bang/jquery-mobile-plugins
 * @author Matt Carter <m@ttcarter.com>
 */
-(function($, window, undefined) {
+$(document).on('mobileinit', function() {
+	console.log('Router started');
+	$.extend($.mobile, {
+		hashListeningEnabled: false
+	});
+});
+$(function() {
 	$(document)
 		.bind('pagebeforechange', function(e, data) {
 			data.params = [];
 			if (typeof data.toPage != 'string')
 				return;
 			var url = (typeof data.toPage == 'string') ? data.toPage : window.location;
-			var urlparsed = /^(.*?)#(.+?)\?(.*)$/.exec(url);
+			var urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(url);
 
 			if (urlparsed) {
 				var baseurl = urlparsed[1];
 				var hash = urlparsed[2];
-				var keypairs = urlparsed[3].split('&');
-				data.paramString = '#' + hash + '?' + keypairs;
+				if (urlparsed[3]) {
+					var keypairs = urlparsed[3].split('&');
+					data.paramString = '#' + hash + '?' + keypairs;
 
-				for (var i = 0; i < keypairs.length; i++) {
-					var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
-					if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
-						data.params[keypairs[i]] = 0;
-					} else // Valid key=val pair
-						data.params[keyval[1]] = keyval[2];
+					for (var i = 0; i < keypairs.length; i++) {
+						var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
+						if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
+							data.params[keypairs[i]] = 0;
+						} else // Valid key=val pair
+							data.params[keyval[1]] = keyval[2];
+					}
 				}
 				data.toPage = $('#' + hash);
 			} else
@@ -33,5 +41,15 @@
 		.bind('pagechange', function(e, data) {
 			if (data.paramString)
 				document.location.hash = data.paramString;
+		})
+		.bind('pageinit', function(e) {
+			if ($.mobile.routerinit)
+				return;
+			$.mobile.routerinit = 1;
+			var urlparsed;
+			if (urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(window.location)) {
+				console.log('Initial route to ' + urlparsed[2]);
+				$.mobile.changePage('#' + urlparsed[2] + (urlparsed[3] ? '?' + urlparsed[3] : ''));
+			}
 		});
-})(jQuery, window);
+});
