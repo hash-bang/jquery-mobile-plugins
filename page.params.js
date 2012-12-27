0b27ef037b7c8cd6b469006f18e5b0d797fdbbf2
Matt Carter <m@ttcarter.com>
2012-12-27 18:29:00 +1000

    Added router version of $.mobile.urlHistory to track page parameters Added support for '#back' href

--- page.params.js
+++ page.params.js
@@ -8,8 +8,29 @@ $(document).on('mobileinit', function() {
 	console.log('Router - Started');
 	$.extend($.mobile, {
 		linkBindingEnabled: false,
-		hashListeningEnabled: false
+		hashListeningEnabled: false,
+		routerBackTransition: 0
 	});
+	// Replace the urlHistory addNew function with our own which stores the page params
+	$.mobile.urlHistory.addNew = function(url, transition, title, pageUrl, role) {
+		if (window.location.hash.substr(1, url.length) == url) // Does the incomming page look like the current hash + params?
+			url = window.location.hash.substr(1); // Replace URL will full window.location URL
+		if ($.mobile.urlHistory.getNext()) // If there's forward history, wipe it
+		     $.mobile.urlHistory.clearForward();
+		$.mobile.urlHistory.stack.push( {url : url, transition: transition, title: title, pageUrl: pageUrl, role: role } );
+		$.mobile.urlHistory.activeIndex = $.mobile.urlHistory.stack.length - 1;
+		console.log('HISTORY NOW', $.mobile.urlHistory.stack);
+	};
+	$.mobile.urlHistory.pop = function(amount) { // Not sure why pop() doesnt exist anyway in the urlHistory object
+		if (!amount)
+			amount = 1;
+		$.mobile.urlHistory.activeIndex -= amount;
+		if ($.mobile.urlHistory.activeIndex <= 0)
+			return {url: 'home'};
+		var prev = $.mobile.urlHistory.stack[$.mobile.urlHistory.activeIndex];
+		$.mobile.urlHistory.stack = $.mobile.urlHistory.stack.slice(0, $.mobile.urlHistory.activeIndex);
+		return prev;
+	};
 });
 $(function() {
 	$(document).bind('pagebeforechange', function(e, data) {
@@ -41,12 +62,27 @@ $(function() {
 		$('#' + hash).trigger('pageprepare', [data]);
 	});
 	$(window).bind('hashchange', function(e) {
-		var urlparsed;
-		if (urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(window.location)) {
-			console.log('Router - Goto ' + urlparsed[2]);
-			$.mobile.changePage('#' + urlparsed[2] + (urlparsed[3] ? '?' + urlparsed[3] : ''), {
-				changeHash: false
-			});
+		if (window.location.hash == '#back') {
+			var lastpage = $.mobile.urlHistory.pop();
+			console.log('Router - Go back', lastpage.url);
+			$.mobile.routerBackTransition = lastpage.transition;
+			window.location.hash = '#' + lastpage.url;
+		} else {
+			if ($.mobile.routerAvoid) {
+				console.log('ROUTER AVOID');
+				$.mobile.routerAvoid--;
+			}
+			var urlparsed;
+			if (urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(window.location)) {
+				console.log('Router - Goto ' + urlparsed[2]);
+				var options = {changeHash: false};
+				if ($.mobile.routerBackTransition) {
+					options.transition = $.mobile.routerBackTransition;
+					options.reverse = 1;
+					$.mobile.routerBackTransition = 0;
+				}
+				$.mobile.changePage('#' + urlparsed[2] + (urlparsed[3] ? '?' + urlparsed[3] : ''), options);
+			}
 		}
 	});
 });
