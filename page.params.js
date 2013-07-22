c11ef73bbd36b3a597c2ac02f01aed2940834302
Matt Carter <m@ttcarter.com>
2013-07-22 12:05:37 +1000

    Moved router trigger into its own function so it can also be manually called

--- page.params.js
+++ page.params.js
@@ -53,55 +53,61 @@ $(document).on('mobileinit', function() {
 		}
 	});
 });
-$(function() {
-	$(document).bind('pagebeforechange', function(e, data) {
-		data.params = [];
-		if (typeof data.toPage != 'string')
-			return;
-		var url = (typeof data.toPage == 'string') ? data.toPage : window.location;
-		var urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(url);
+$(document).bind('pagebeforechange', function(e, data) {
+	data.params = [];
+	if (typeof data.toPage != 'string')
+		return;
+	var url = (typeof data.toPage == 'string') ? data.toPage : window.location;
+	var urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(url);
 
-		if (urlparsed) {
-			var baseurl = urlparsed[1];
-			var hash = urlparsed[2];
-			if (urlparsed[3]) {
-				var keypairs = urlparsed[3].split('&');
-				data.paramString = '#' + hash + '?' + keypairs;
+	if (urlparsed) {
+		var baseurl = urlparsed[1];
+		var hash = urlparsed[2];
+		if (urlparsed[3]) {
+			var keypairs = urlparsed[3].split('&');
+			data.paramString = '#' + hash + '?' + keypairs;
 
-				for (var i = 0; i < keypairs.length; i++) {
-					var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
-					if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
-						data.params[keypairs[i]] = 0;
-					} else // Valid key=val pair
-						data.params[keyval[1]] = keyval[2];
-				}
+			for (var i = 0; i < keypairs.length; i++) {
+				var keyval = /^(.*?)=(.*)$/.exec(keypairs[i]);
+				if (!keyval || keyval.length < 2) { // Deal with parameters without values (e.g. '#hash?param1=value1&param2' where 'param2' does not have a value but is still there)
+					data.params[keypairs[i]] = 0;
+				} else // Valid key=val pair
+					data.params[keyval[1]] = keyval[2];
 			}
-			data.toPage = '#' + hash;
-		} else
-			data.paramString = false;
+		}
+		data.toPage = '#' + hash;
+	} else
+		data.paramString = false;
 
-		$('#' + hash).trigger('pageprepare', [data]);
-	});
-	$(window).bind('hashchange', function(e) {
-		if ($.mobile.routerAvoid) {
-			$.mobile.routerAvoid--;
-			return;
-		}
-		var urlparsed;
-		if (urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(window.location)) {
-			console.log('Router - Goto ' + urlparsed[2]);
-			var options = {changeHash: false};
-			if ($.mobile.routerBackTransition) {
-				options.transition = $.mobile.routerBackTransition;
-				options.reverse = true;
-				$.mobile.routerBackTransition = false;
-			} else if ($.mobile.routerA) {
-				var aTransition = $.mobile.routerA.data('transition');
-				if (aTransition)
-					options.transition = aTransition;
-			}
-			$.mobile.changePage('#' + urlparsed[2] + (urlparsed[3] ? '?' + urlparsed[3] : ''), options);
-		}
-		
-	});
+	console.log('Router - Trigger pareprepare on #' + hash);
+	$('#' + hash).trigger('pageprepare', [data]);
 });
+
+$.routerTrigger = function(e) {
+	if (!$.mobile) {
+		console.log('Router called before jQMobile load');
+		return;
+	}
+	if ($.mobile.routerAvoid) {
+		$.mobile.routerAvoid--;
+		return;
+	}
+	var urlparsed;
+	if (urlparsed = /^(.*?)#(.+?)(?:\?(.*))?$/.exec(window.location)) {
+		console.log('Router - Goto ' + urlparsed[2]);
+		var options = {changeHash: false};
+		if ($.mobile.routerBackTransition) {
+			options.transition = $.mobile.routerBackTransition;
+			options.reverse = true;
+			$.mobile.routerBackTransition = false;
+		} else if ($.mobile.routerA) {
+			var aTransition = $.mobile.routerA.data('transition');
+			if (aTransition)
+				options.transition = aTransition;
+		}
+		$.mobile.changePage('#' + urlparsed[2] + (urlparsed[3] ? '?' + urlparsed[3] : ''), options);
+	}
+	
+};
+$(window).on('hashchange', $.routerTrigger);
+$.routerTrigger();
